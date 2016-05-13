d49f9e2c6d80cba60a1684ec7cb1843c645d8360
John <jldjld503@gmail.com>
2016-05-13 08:49:46 -0700

    all() empty at first

--- src/test/java/BandTest.java
+++ src/test/java/BandTest.java
@@ -6,18 +6,35 @@ import java.util.List;
 
 public class BandTest {
 
-  @Rule
-  public DatabaseRule database = new DatabaseRule();
-
-  @Test
-  public void Band_instantiatesCorrectly() {
-    Band myBand = new Band("Children of the Corn", "Jazz");
-    assertTrue(myBand instanceof Band);
-  }
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @Test
-    public void getBandName_returnsBandName() {
+    public void Band_instantiatesCorrectly() {
+        Band myBand = new Band("Children of the Corn", "Jazz");
+        assertTrue(myBand instanceof Band);
+    }
+
+    @Test
+    public void getBandName_returnsBandName_String() {
         Band myBand = new Band("Children of the Corn", "Jazz");
         assertEquals("Children of the Corn", myBand.getBandName());
+    }
+
+    @Test
+    public void getGenre_returnsGenre_String() {
+        Band myBand = new Band("Children of the Corn", "Jazz");
+        assertEquals("Jazz", myBand.getGenre());
+    }
+
+    @Test
+    public void getId_returnsId_int() {
+        Band myBand = new Band("Children of the Corn", "Jazz");
+        assertEquals(0, myBand.getId());
+    }
+
+    @Test
+    public void all_emptyAtFirst() {
+        assertEquals(Band.all().size(), 0);
     }
 }
