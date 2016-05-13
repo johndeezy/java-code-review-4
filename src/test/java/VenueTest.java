1b5430debb7f87a28f179a0f475ea3af195d5eca
John <jldjld503@gmail.com>
2016-05-13 09:38:54 -0700

    object Venue instantiates

--- src/test/java/VenueTest.java
+++ src/test/java/VenueTest.java
@@ -6,12 +6,53 @@ import java.util.List;
 
 public class VenueTest {
 
-  @Rule
-  public DatabaseRule database = new DatabaseRule();
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
-  // @Test
-  // public void Venue_instantiatesCorrectly() {
-  //   Venue myVenue = new Venue("Venue 1");
-  //   assertTrue(myVenue instanceof Venue);
-  // }
+    @Test
+    public void Venue_objectInstantiatesCorrectly() {
+        Venue myVenue = new Venue("Venue", "Dallas", "Texas", 100);
+        assertTrue(myVenue instanceof Venue);
+    }
+
+    // @Test
+    // public void getFullName_returnsFirstNameAndLastName_String() {
+    //     Author myAuthor = new Author("Tim", "Anker");
+    //     assertEquals("Tim Anker", myAuthor.getFullName());
+    // }
+    //
+    // @Test
+    // public void all_emptyAtFirst() {
+    //     assertEquals(Author.all().size(), 0);
+    // }
+    //
+    // @Test
+    // public void equals_returnsTrueifTitlesAreTheSame() {
+    //     Author firstAuthor = new Author("Author 1", "last");
+    //     Author secondAuthor = new Author("Author 1", "last");
+    //     assertTrue(firstAuthor.equals(secondAuthor));
+    // }
+    //
+    // @Test
+    // public void save_returnsTrueifTitlesAreTheSame() {
+    //     Author firstAuthor = new Author("Author 1", "last");
+    //     firstAuthor.save();
+    //     assertEquals(Author.all().get(0), firstAuthor);
+    // }
+    //
+    // @Test
+    // public void save_assignsIdToObject_1() {
+    //     Author firstAuthor = new Author("Author 1", "last");
+    //     firstAuthor.save();
+    //     Author savedAuthor = Author.all().get(0);
+    //     assertEquals(savedAuthor.getId(), firstAuthor.getId());
+    // }
+    //
+    // @Test
+    // public void find_findsAuthorInDatabase_true() {
+    //     Author myAuthor = new Author("Author1", "last");
+    //     myAuthor.save();
+    //     Author savedAuthor = Author.find(myAuthor.getId());
+    //     assertTrue(myAuthor.equals(savedAuthor));
+    // }
 }
