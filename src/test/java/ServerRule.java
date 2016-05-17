97f2b1c3e42175eacb805fb61b0d1163a46a933b
John <jldjld503@gmail.com>
2016-05-17 07:28:20 -0700

    fixed indenting

--- src/test/java/ServerRule.java
+++ src/test/java/ServerRule.java
@@ -3,12 +3,12 @@ import spark.Spark;
 
 public class ServerRule extends ExternalResource {
 
-  protected void before() {
-    String[] args = {};
-    App.main(args);
-  }
-  
-  protected void after() {
-    Spark.stop();
-  }
+    protected void before() {
+        String[] args = {};
+        App.main(args);
+    }
+
+    protected void after() {
+        Spark.stop();
+    }
 }
